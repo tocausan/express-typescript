dfe6fb7dfcc886531bbdf7f6fc5d6cc20d39ed08
tocausan <tocausan@gmail.com>
2018-04-05 21:52:47 +0200

    typescript refactoring

---
+++ server/src/bin/www.ts
@@ -0,0 +1,55 @@
+import * as debug from 'debug';
+import * as http from 'http';
+import App from '../app';
+import {Config} from "../config";
+
+
+// Get port from environment and store in Express.
+const port = normalizePort(process.env.PORT || Config.server.port);
+App.set('port', port);
+
+// Create HTTP server.
+const server = http.createServer(App)
+    .listen(port)
+    .on('error', onError)
+    .on('listening', onListening)
+
+// Normalize a port into a number, string, or false.
+function normalizePort(val: any) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
+}
+
+// Event listener for HTTP server "error" event.
+function onError(error: any) {
+    if (error.syscall !== 'listen') throw error;
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+// Event listener for HTTP server "listening" event.
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
---
+++ server/src/config.ts
@@ -0,0 +1,5 @@
+export const Config = {
+    server: {
+        port: 3000
+    }
+};

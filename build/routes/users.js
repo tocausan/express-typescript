bfaad2ee28a90554aee0b720849998b8934ba516
tocausan <tocausan@gmail.com>
2018-04-07 16:02:05 +0200

    clean

--- build/routes/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-exports.UserRoutes = express_1.Router()
-    .get('/', function (req, res, next) {
-    res.json('users');
-});
